e74b00e1827023e1d4b7e8a1841e75f660291ac6
Sridaran04dec <sri04122001@gmail.com>
2024-11-21 11:21:48 +0530

    updated the mail template

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -287,106 +287,358 @@ $(document).ready(function () {
         const payload = new FormData();
         payload.append("from", "ecscloudinfotech@gmail.com");
         payload.append("to", modalData.email);
-        payload.append("subject", `Secure Your Spot: Complete Your ${modalData.course} Payment Today!`);
+        payload.append(
+          "subject",
+          `Secure Your Spot: Complete Your ${modalData.course} Payment Today!`
+        );
         payload.append(
           "htmlContent",
           `
-      <!DOCTYPE html>
-      <html lang="en">
-        <head>
-          <meta charset="UTF-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <title>Payment Request - Course Registration</title>
-          <style>
-            /* Your styles here */
-          </style>
-        </head>
-        <body>
-          <div class="container">
-            <div class="header" style="padding: 25px; border-bottom: 1px solid #ddd; text-align: center">
-              <table width="100%" cellspacing="0" cellpadding="0" style="border-collapse: collapse">
-                <tr>
-                  <td align="left" style="vertical-align: middle">
-                    <img src="https://ecsaio.in/assets/images/logo.webp" alt="Deals Machi" style="max-width: 150px; height: auto" />
-                  </td>
-                  <td align="right" style="vertical-align: middle">
-                    <div class="headerText" style="font-size: 14px; color: #333">
-                      <a href="https://ecsaio.com/" target="_blank" style="text-decoration: none; color: #333">
-                        Your <span style="color: #005aea">info@ecsaio.in</span>
-                      </a> | <a href="tel:919150150687" target="_blank" style="text-decoration: none; color: #333">
-                        +91 9150150687
-                      </a>
-                    </div>
-                  </td>
-                </tr>
-              </table>
-            </div>
-            <div class="message">
-              <p>Hello ${modalData.name}!</p>
-              <p>Thank you for registering for the course <strong> ${
-                modalData.course
-              }</strong> on our platform. Below are the details of your registration and payment request.</p>
-            </div>
-            <div class="user-details">
-              <h2>User Details</h2>
-              <p class="sub-heading">Name: <span>${modalData.name}</span></p>
-              <p class="sub-heading">Email: <span>${modalData.email}</span></p>
-              <p class="sub-heading">Phone Number: <span>+91 ${
-                modalData.phoneNumber
-              }</span></p>
-              <p class="sub-heading">Address: <span>${modalData.city}</span></p>
-            </div>
-            <div class="course-details">
-              <h2>Course Details</h2>
-              <p class="course_details">Course Title: ${modalData.course}</p>
-              <div class="fee_details">
-                <div>Amount :</div>
-                <div>${modalData.course_fee}</div>
+     <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Payment Request - Course Registration</title>
+    <style>
+      body {
+        background-color: #f7f9fc;
+        font-family: "Arial", sans-serif;
+        color: #333;
+        margin: 0;
+        padding: 0;
+      }
+
+      .container {
+        max-width: 650px;
+        background-color: #ffffff;
+        margin: 40px auto;
+        box-shadow: 0 4px 15px rgba(0, 0, 0, 0.1);
+        border-top: 5px solid #0076f7;
+        border-bottom: 3px solid #888888;
+      }
+
+      .header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 25px;
+        box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.1);
+      }
+
+      .header img {
+        width: 120px;
+      }
+
+      .headerText a {
+        font-size: 12px;
+        text-decoration: none;
+        color: #000;
+      }
+
+      .message {
+        padding: 10px 25px;
+        box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.1);
+      }
+
+      .message a {
+        color: #0076f7;
+      }
+
+      .content {
+        padding: 0px 25px;
+        line-height: 1.6;
+      }
+
+      .content h4 {
+        color: #0076f7;
+        font-size: 28px;
+        margin-top: 15px;
+        margin-bottom: 15px;
+      }
+
+      .content p {
+        font-size: 15px;
+        margin-bottom: 20px;
+      }
+
+      .vendor-details {
+        margin: 20px 0;
+        padding: 15px;
+        border: 1px solid #ddd;
+        background-color: #f9f9f9;
+      }
+
+      .vendor-details h2 {
+        color: #333;
+        font-size: 20px;
+        margin-bottom: 10px;
+        margin-top: 0px;
+      }
+
+      .vendor-details .col-6 {
+        flex: 0 0 50%;
+        max-width: 50%;
+      }
+
+      .vendor-details p {
+        margin: 0px;
+      }
+
+      .vendor-details .sub-heading span {
+        color: #6b7b93;
+      }
+
+      .cta-button {
+        text-align: center;
+        margin: 40px 0;
+      }
+
+      .cta-button a {
+        background-color: #0076f7;
+        color: #fff;
+        padding: 14px 28px;
+        font-size: 16px;
+        text-decoration: none;
+        border-radius: 6px;
+        box-shadow: 0 4px 10px rgba(255, 153, 0, 0.2);
+        transition: background-color 0.3s;
+      }
+
+      .cta-button a:hover {
+        background-color: #cc3b3b;
+      }
+      .user-details {
+        padding: 15px 25px;
+      }
+      .course-details {
+        padding: 15px 25px;
+      }
+
+      .align_button {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+      .align_button input {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 15px;
+        background-color: #0076f7;
+        color: #fff;
+        border: 1px solid #0076f7;
+        border-radius: 6px;
+      }
+      .align_button input:hover {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 15px;
+        background-color: #fff;
+        color: #0076f7;
+        border: 1px solid #0076f7;
+        border-radius: 6px;
+      }
+      .fee_details {
+        display: flex;
+        justify-content: space-between !important;
+        align-items: center;
+        padding: 15px 0;
+      }
+
+      .footer img {
+        width: 120px;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="container">
+      <div
+        class="header"
+        style="padding: 25px; border-bottom: 1px solid #ddd; text-align: center"
+      >
+        <table
+          width="100%"
+          cellspacing="0"
+          cellpadding="0"
+          style="border-collapse: collapse"
+        >
+          <tr>
+            <td align="left" style="vertical-align: middle">
+              <img
+                src="https://ecsaio.in/assets/images/logo.webp"
+                alt="Deals Machi"
+                style="max-width: 150px; height: auto"
+              />
+            </td>
+            <td align="right" style="vertical-align: middle">
+              <div class="headerText" style="font-size: 14px; color: #333">
+                <a
+                  href="https://ecsaio.com/"
+                  target="_blank"
+                  style="text-decoration: none; color: #333"
+                >
+                  Your <span style="color: #005aea">info@ecsaio.in</span>
+                </a>
+                |
+                <a
+                  href="tel:919150150687"
+                  target="_blank"
+                  style="text-decoration: none; color: #333"
+                >
+                  +91 9150150687
+                </a>
               </div>
-              <div class="fee_details">
-                <div>GST :</div>
-                <div>${modalData.course_gst}</div>
-              </div>
-              <hr />
-              <div class="fee_details">
-                <div>Total :</div>
-                <div>${(
-                  parseFloat(modalData.course_fee) +
-                  parseFloat(modalData.course_gst)
-                ).toFixed(0)}</div>
-              </div>
-            </div>
-            <div class="align_button">
-              <form name="sdklaunch" id="sdklaunch" action="${
-                response.href
-              }" method="POST">
-                <input type="hidden" id="bdorderid" name="bdorderid" value="${
-                  response.bdOrderId
-                }" />
-                <input type="hidden" id="merchantid" name="merchantid" value="${
-                  response.merchantId
-                }" />
-                <input type="hidden" id="rdata" name="rdata" value="${
-                  response.rdata
-                }" />
-                <input name="submit" type="submit" class="cta-button" value="Complete your Payment" />
-              </form>
-            </div>
-            <div class="footer" style="padding: 15px 25px; text-align: center">
-              <table width="100%" cellspacing="0" cellpadding="0" style="border-collapse: collapse">
-                <tr>
-                  <td align="left" style="vertical-align: middle">
-                    <img src="https://ecsaio.in/assets/images/logo.webp" alt="dealsmachi" style="max-width: 150px; height: auto; margin-bottom: 10px" />
-                  </td>
-                  <td align="right" style="vertical-align: middle">
-                    <p style="font-size: 12px; color: #333; margin: 0">Connect with <a href="https://ecsaio.com/" target="_blank" style="color: #005aea; text-decoration: none">cloud ECS infotech</a> India</p>
-                  </td>
-                </tr>
-              </table>
-            </div>
-          </div>
-        </body>
-      </html>
+            </td>
+          </tr>
+        </table>
+      </div>
+      <div class="message">
+        <p>Hello ${modalData.name}!</p>
+        <p>
+          Thank you for registering for the course
+          <strong> ${modalData.course}</strong> on our platform. Below are the
+          details of your registration and payment request.
+        </p>
+      </div>
+      <div class="user-details">
+        <h2>User Details</h2>
+        <p class="sub-heading">Name: <span>${modalData.name}</span></p>
+        <p class="sub-heading">Email: <span>${modalData.email}</span></p>
+        <p class="sub-heading">
+          Phone Number: <span>+91 ${modalData.phoneNumber}</span>
+        </p>
+        <p class="sub-heading">Address: <span>${modalData.city}</span></p>
+      </div>
+      <div class="course-details">
+        <h2>Course Details</h2>
+        <p class="course_details">Course Title: ${modalData.course}</p>
+
+        <table
+          width="100%"
+          cellspacing="0"
+          cellpadding="0"
+          style="border-collapse: collapse"
+        >
+          <tr>
+            <td align="left" style="vertical-align: middle; padding: 15px 0">
+              Amount :
+            </td>
+            <td align="right" style="vertical-align: middle">
+              ${modalData.course_fee}
+            </td>
+          </tr>
+          <tr>
+            <td align="left" style="vertical-align: middle; padding: 15px 0">
+              GST :
+            </td>
+            <td align="right" style="vertical-align: middle">
+              ${modalData.course_gst}
+            </td>
+          </tr>
+        </table>
+        <hr />
+        <table
+          width="100%"
+          cellspacing="0"
+          cellpadding="0"
+          style="border-collapse: collapse"
+        >
+          <tr>
+            <td align="left" style="vertical-align: middle; padding: 15px 0">
+              Total :
+            </td>
+            <td align="right" style="vertical-align: middle">
+              ${(
+                parseFloat(modalData.course_fee) +
+                parseFloat(modalData.course_gst)
+              ).toFixed(0)}
+            </td>
+          </tr>
+        </table>
+      </div>
+
+      <div class="align_button" style="text-align: center; padding: 20px">
+        <form
+          name="sdklaunch"
+          id="sdklaunch"
+          action="${response.href}"
+          method="POST"
+        >
+          <input
+            type="hidden"
+            id="bdorderid"
+            name="bdorderid"
+            value="${response.bdOrderId}"
+          />
+          <input
+            type="hidden"
+            id="merchantid"
+            name="merchantid"
+            value="${response.merchantId}"
+          />
+          <input
+            type="hidden"
+            id="rdata"
+            name="rdata"
+            value="${response.rdata}"
+          />
+          <div style="text-align:center;">
+          <input
+            name="submit"
+            type="submit"
+            style="
+              display: inline-block;
+              padding: 14px 28px;
+              background-color: #0076f7;
+              color: #fff;
+              font-size: 16px;
+              text-decoration: none;
+              border-radius: 6px;
+              border: none;
+              cursor: pointer;
+              box-shadow: 0 4px 10px rgba(255, 153, 0, 0.2);
+              transition: background-color 0.3s;
+            "
+            value="Complete your Payment"
+          /></div>
+        </form>
+      </div>
+      <div class="footer" style="padding: 15px 25px; text-align: center">
+        <table
+          width="100%"
+          cellspacing="0"
+          cellpadding="0"
+          style="border-collapse: collapse"
+        >
+          <tr>
+            <td align="left" style="vertical-align: middle">
+              <img
+                src="https://ecsaio.in/assets/images/logo.webp"
+                alt="dealsmachi"
+                style="max-width: 150px; height: auto; margin-bottom: 10px"
+              />
+            </td>
+            <td align="right" style="vertical-align: middle">
+              <p style="font-size: 12px; color: #333; margin: 0">
+                Connect with
+                <a
+                  href="https://ecsaio.com/"
+                  target="_blank"
+                  style="color: #005aea; text-decoration: none"
+                  >cloud ECS infotech</a
+                >
+                India
+              </p>
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  </body>
+</html>
+
     `
         );
 
